8d2c3912a56afbe833f8612b26b27f16e15aa858
Diego de los Reyes <diegorys@gmail.com>
2020-07-12 16:34:37 +0200

    Primera versión de la funcionalidad emocional de BallY.

---
+++ Ximies/Bally/V2.0/2_Software/3_RaspberryPiZeroW/emociones/emociones.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+'''
+- Proyecto: Bally 2.0
+- Nombre del desarrollador: Diego de los Reyes Rodríguez (diegorys)
+- Para: Xpikuos
+- Version: 20.07.12.16.10.00
+- Descripción: Representa el estado emocional actual de BallY. Tiene un listado de emociones, cada una con una determinada intensidad.
+- Licencia: Este trabajo está licenciado bajo la licencia de Atribución-NoComercialCompartirIgual 4.0 Internacional (CC BY-NC-SA 4.0)
+    Para ver una copia de esta licencia, visita
+    https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es
+'''
+
+
+class Emociones():
+
+    def actuar(self):
+        print("In progress")
+
+
+if __name__ == "__main__":
+    emociones = Emociones()
+    emociones.actuar()

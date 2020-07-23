e2880d8339a6f6c976c5a94dd0392f0b6c8be420
Diego de los Reyes <diegorys@gmail.com>
2020-07-23 20:34:14 +0200

    Hecho con Sentiment Classifier en español.

--- Ximies/Bally/V2.0/2_Software/3_RaspberryPiZeroW/emociones_texto/emociones_texto.py
+++ Ximies/Bally/V2.0/2_Software/3_RaspberryPiZeroW/emociones_texto/emociones_texto.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import os
 import sys
+from classifier import SentimentClassifier
 
 '''
 - Proyecto: Bally 2.0
 - Nombre del desarrollador: Diego de los Reyes Rodríguez (diegorys)
 - Para: Xpikuos
-- Version: 20.07.12.16.15.00
-- Descripción: Analiza un texto y busca emociones en él.
+- Version: 20.07.20.18.36.00
+- Descripción: Analiza un texto en español y comprueba si el sentimiento que genera es positivo o negativo.
+El resultado es un número decimal entre 0 y 1.
+    Cuanto más cercano a 0, más negativo.
+    Cuanto más cercano a 1, más positivo.
+    Cuanto más cercano a 0.5, más neutro.
+Para instalarlo, previamente hay que instalar las siguientes dependencias:
+    sudo pip3 install spanish_sentiment_analysis
+Puedes probarlo con:
+python3 emociones_texto.py "Texto a probar"
 - Licencia: Este trabajo está licenciado bajo la licencia de Atribución-NoComercialCompartirIgual 4.0 Internacional (CC BY-NC-SA 4.0)
     Para ver una copia de esta licencia, visita
     https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es
@@ -18,8 +26,13 @@ import sys
 
 class EmocionesTexto():
 
-    def actuar(self, texto):
-        print('Buscando emociones en: ' + texto)
+    def __init__(self):
+        self.clf = SentimentClassifier()
+
+    def actuar(self, text):
+        print(text)
+        sentimiento = self.clf.predict(text)
+        print(sentimiento)
 
 
 if __name__ == "__main__":
